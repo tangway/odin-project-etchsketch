967564488683d7b84812f097e87ce2437d6689bc
tangwwwei <20863759+tangwwwei@users.noreply.github.com>
2023-01-09 10:46:57 +0800

    Implement longer fade and and fade out trail

--- script.js
+++ script.js
@@ -7,6 +7,10 @@
 // then make a whole row of 16 of it
 // then think of how to make it vertically downwards
 
+// make input button that will allow positive numbers of 1 - 100 to be entered
+// this means that once the number is entered the screen has to be redrawn
+// and the input box be emptied 
+
 // const createGrid = document.createElement('div')
 // createGrid.style.width = "20px"
 // createGrid.style.height = "20px"
@@ -17,7 +21,7 @@
 // container.appendChild(createGrid)
 
 
-/* Example solution to 16x16 grid */
+
 const container = document.getElementById("container");
 
 function makeRows(grids) {
@@ -26,12 +30,16 @@ function makeRows(grids) {
   for (c = 0; c < (grids * grids); c++) {
     let cell = document.createElement("div");
     // cell.innerText = (c + 1);
-    // cell.addEventListener("mouseover", () => cell.style.backgroundColor = "white")
-    // cell.addEventListener("mouseout", () => cell.style.backgroundColor = "lightslategrey")
-
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(10);
-///
+
+const enteredValue = document.querySelector('input[type="number"]')
+enteredValue.addEventListener("keydown", (e) => {
+const num = enteredValue.valueAsNumber
+  if ((e.code === "Enter") && (1 < num && num < 101)) {
+    container.innerHTML = ""
+    makeRows(num)
+  }
+})
